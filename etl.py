40499a72182909ccff46d215e6a5bb504bc85e6b
magistraapta <magsitraagis@gmail.com>
2024-08-21 15:11:48 +0800

    change token

--- etl.py
+++ etl.py
@@ -3,5 +3,5 @@ import pandas as pd
 import psycopg2
 
 API_URL = "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb"
-API_TOKEN = "BQBCHzpWD5C0eGWjHkCWePhdZaexcgCAn0536RHhpt523AgkTDs0FhTKKgnl9Ub6YpqUCsDteRWtbg6S4HNtSo86H1MZm1OkaFtY-cXsD6-RTPlDdwU"
+API_TOKEN = "token"
 
--- spotify_api.py
+++ spotify_api.py
@@ -2,7 +2,7 @@ import requests
 class SpotifyApi:
 
     API_URL = "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb"
-    API_TOKEN = "BQBCHzpWD5C0eGWjHkCWePhdZaexcgCAn0536RHhpt523AgkTDs0FhTKKgnl9Ub6YpqUCsDteRWtbg6S4HNtSo86H1MZm1OkaFtY-cXsD6-RTPlDdwU"
+    API_TOKEN = "token"
 
     def __init__(self, url):
         self.url = url
